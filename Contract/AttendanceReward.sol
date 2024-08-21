dcec9637fe383e2c440a96e9f08d9e54599fab66
Shidharth Laishram <139545014+shwwid@users.noreply.github.com>
2024-08-21 13:56:52 +0530

    Update AttendanceReward.sol

--- Contract/AttendanceReward.sol
+++ Contract/AttendanceReward.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 contract AttendanceReward {
 
-    // Data Structure containing variables and their datatype to hold student information
+    
     struct Student {
         uint attendanceCount;
         uint rewardBalance;
@@ -11,12 +11,12 @@ contract AttendanceReward {
 
     mapping(address => Student) public students;
 
-    // Token reward per attendance as the Student check's in
+    
     uint public rewardPerAttendance = 5;
 
     event CheckIn(address indexed studentAddress, uint attendanceCount, uint rewardTokens);
 
-    // Function to record attendance and reward their attendance using the token.
+    
     function checkIn() public {
        
         students[msg.sender].attendanceCount += 1;
@@ -26,7 +26,7 @@ contract AttendanceReward {
         emit CheckIn(msg.sender, students[msg.sender].attendanceCount, students[msg.sender].rewardBalance);
     }
 
-    // Function to get student details using their Ethereum Address by checking from the memory
+    
     function getStudent(address _studentAddress) public view returns (uint, uint) {
         Student memory student = students[_studentAddress];
         return (student.attendanceCount, student.rewardBalance);
